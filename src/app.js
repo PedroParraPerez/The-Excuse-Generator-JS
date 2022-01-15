65492ff2cdb9199c6d40751f51b331c6700ebf3f
PedroParraPerez <pedroparra06091994@gmail.com>
2022-01-15 12:14:23 +0000

    optimization

--- src/app.js
+++ src/app.js
@@ -8,9 +8,16 @@ import "./assets/img/4geeks.ico";
 let who = ["The dog", "My grandma", "His turtle", "My bird"];
 let action = ["ate", "peed", "crushed", "broke"];
 let what = ["my homework", "the keys", "the car"];
-let when = ["before the class","right on time","when I finished","during my lunch","while I was praying"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying",
+];
 
 const UnArrayAleatorio = (word0, word1, word2, word3) => {
+
   let NumberRandom0 = Math.floor(Math.random() * word0.length);
   let NumberRandom1 = Math.floor(Math.random() * word1.length);
   let NumberRandom2 = Math.floor(Math.random() * word2.length);
@@ -23,14 +30,14 @@ const UnArrayAleatorio = (word0, word1, word2, word3) => {
 
 UnArrayAleatorio(who, action, what, when);
 
-// Willy
+// OPTIMIZATION ()
 
-// const UnArrayAleatorio1 = (array, array1, array2, array3) => {
+const UnArrayAleatorio1 = (word0, word1, word2, word3) => {
+  let NumberRandom = Math.floor(Math.random() * word3.length);
 
-//   let NumberRandom0 = Math.floor(Math.random() * array.length);
+  return console.log(
+    `${word0[NumberRandom]} ${word1[NumberRandom]} ${word2[NumberRandom]} ${word3[NumberRandom]}`
+  );
+};
 
-//   return console.log(`${array[NumberRandom0]} ${array1[NumberRandom0]} ${array2[NumberRandom0]} ${array3[NumberRandom0]}`);
-
-// }
-
-// UnArrayAleatorio1(who, action, what, when);
+UnArrayAleatorio1(who, action, what, when);
