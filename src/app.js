44d6363a217a08ed16c9ce08da8054c4a0d8be2e
PedroParraPerez <pedroparra06091994@gmail.com>
2022-01-17 14:20:50 +0000

    Optimizacion

--- src/app.js
+++ src/app.js
@@ -1,10 +1,3 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 let who = ["The dog", "My grandma", "His turtle", "My bird"];
 let action = ["ate", "peed", "crushed", "broke"];
 let what = ["my homework", "the keys", "the car"];
@@ -13,13 +6,16 @@ let when = [
   "right on time",
   "when I finished",
   "during my lunch",
-  "while I was praying",
+  "while I was praying"
 ];
+let ArrayOfArrays = [["The dog", "My grandma", "His turtle", "My bird"]];
+
 // Dos formas de hacer el ejercicio, la primera "UnArrayAleatorio" con la imposibilidad de realizar testeos de la funcion ya que la funcion tiene dos funciones asignadas
 // La segunda forma, dividimos la funcion en dos funciones (Una para cada tarea) de tal forma que podremos realizar testeos.
 // Siempre tenemos que tratar que una funcion tenga como trabajo realziar UNA sola funcion.
+// Finalmente generar unas funciones que independientemente del numero de arrays que tenga, me genere la escusa
 
-const UnArrayAlea = (array) => {
+const UnArrayAlea = array => {
   let NumberRandom0 = Math.floor(Math.random() * array.length);
 
   return array[NumberRandom0];
@@ -31,15 +27,8 @@ console.log(
   )} ${UnArrayAlea(when)}`
 );
 
-const RandomIndex = (arr) => Math.floor(Math.random() * arr.length);
+const RandomIndex = arr => Math.floor(Math.random() * arr.length);
 const RandomItem = (arr, i) => arr[i];
-
-console.log(
-  `${RandomItem(who, RandomIndex(who))} ${RandomItem(
-    action,
-    RandomIndex(action)
-  )} ${RandomItem(what, RandomIndex(what))} ${RandomItem(
-    when,
-    RandomIndex(when)
-  )}`
-);
+const Arrays = arr => {
+  for (i = 0; i < ArrayOfArrays.length; i++) {}
+};
